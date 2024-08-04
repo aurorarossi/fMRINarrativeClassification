fe9b4c3327139b73588cf9549085e21864d59ddc
aurorarossi <aurora98rossi@gmail.com>
2024-08-04 22:12:20 +0200

    Inter intra std

--- utils.jl
+++ utils.jl
@@ -1,4 +1,4 @@
-using DelimitedFiles, Flux, NPZ
+using DelimitedFiles, Flux, NPZ, StatsBase
 
 function load_names_file()
     name1 = readdlm("files/to_process/to_process_schema1.txt")
@@ -7,6 +7,32 @@ function load_names_file()
     name4 = readdlm("files/to_process/to_process_schema4.txt")
     return vcat(name1, name2, name3, name4)
 end
+
+function select_subjects(graphs, labels, neurons)
+    subjects = sample(1:31, 6, replace=false)
+    println(subjects)
+    subjecttrain = setdiff(1:31,subjects)
+    testset = zeros(100, 100, 8, 1, 96)
+    testlabel = zeros(neurons,96)
+    trainset = zeros(100, 100, 8, 1, 400)
+    trainlabel = zeros(neurons,400)
+    m=1
+    
+    for i in subjecttrain
+        trainset[:, :, :, 1, m:m+15] = graphs[:, :, :, 1, (i-1)*16+1:(i-1)*16+16]
+        trainlabel[:, m:m+15] = labels[:, (i-1)*16+1:(i-1)*16+16]
+        m += 16
+    end
+    testset = zeros(100, 100, 8, 1, 96)
+    k=1
+    for i in subjects
+        testset[:, :, :, 1, k:k+15] = graphs[:, :, :, 1, (i-1)*16+1:(i-1)*16+16]
+        testlabel[:, k:k+15] = labels[:, (i-1)*16+1:(i-1)*16+16]
+        k += 16
+    end
+    return trainset, testset, trainlabel, testlabel
+end
+
 
 function readgraphs_4C!(g, graphs, labels, k, numberschema, numbersubject)
     datal = readdlm("files/airport_restaurant_labels/$(numbersubject)_schema-0$(numberschema)_events.txt")[1]
@@ -73,6 +99,43 @@ function eval_loss_accuracy(model, data_loader, lossfunction)
     loss = lossfunction(ŷ, y)
     acc = round(100 * mean(Flux.onecold(ŷ) .== Flux.onecold(y)); digits=2)
     return (loss=loss, acc=acc)
+end
+
+
+function eval_loss_accuracy_inter_intra_subject(model, data_loader, lossfunction)
+    (x, y) = first(data_loader)
+    ŷ = model(x)
+    loss = lossfunction(ŷ, y)
+    
+    num_subjects = 6
+    data_points_per_subject = 16
+    
+    accuracies_subjects = Float64[]
+    std_subjects = Float64[]
+    
+    for i in 1:num_subjects
+        start_idx = (i - 1) * data_points_per_subject + 1
+        end_idx = i * data_points_per_subject
+        subject_ŷ = ŷ[:, start_idx:end_idx]
+        subject_y = y[:, start_idx:end_idx]
+        subject_acc = mean(Flux.onecold(subject_ŷ) .== Flux.onecold(subject_y))
+        subject_std = std(Flux.onecold(subject_ŷ) .== Flux.onecold(subject_y))
+        push!(accuracies_subjects, subject_acc)
+        push!(std_subjects, subject_std)
+    end
+    
+    avg_acc_intra = round(100 * mean(accuracies_subjects); digits=2)
+    std_acc_intra = round(100 * mean(std_subjects); digits=2)
+    std_acc_inter = round(100 * std(accuracies_subjects); digits=2)
+    total_std = std(Flux.onecold(ŷ) .== Flux.onecold(y))
+    
+    return (loss=loss, acc_intra=avg_acc_intra, std_acc_intra=std_acc_intra, std_acc_inter=std_acc_inter, total_std=total_std)
+end
+
+function report_inter_intra(epoch, model,train_loader, test_loader, lossfunction)
+    loss, acc_intra, std_acc_intra, std_acc_inter, total_std = eval_loss_accuracy_inter_intra_subject(model, test_loader, lossfunction)
+    println("Epoch: $epoch $((; loss, acc_intra, std_acc_intra, std_acc_inter))")
+    return  acc_intra, std_acc_intra, std_acc_inter, total_std
 end
 
 function report(epoch, model, train_loader, test_loader, lossfunction)
