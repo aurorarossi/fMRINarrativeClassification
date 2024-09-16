c0667daf7ee9ffc3e67aa73c8b4be3bd943bfea0
Aurora Rossi <aurora98rossi@gmail.com>
2024-09-16 17:17:55 +0200

    ok

--- movie_audio_classification/plot_shap.jl
+++ movie_audio_classification/plot_shap.jl
@@ -1,17 +1,30 @@
 using Random, CairoMakie, Statistics, LaTeXStrings
 using JLD2 
-include("/user/aurossi/home/fMRINarrativeClassification/plots/plot.jl")
+
 include("../plots/plot.jl")
-include("../t-test/t-test_17.jl")
-include("../t-test/t-test.jl")
 
-data7 = load("movie_audio_classification/data/shapleyvalues7_15retraining_changeshuffling.jld2")
-p = shapvalues_plot(data7, L"Modality classification$$","png")
-display(p)
-save("movie_audio_classification/plots/new/shapleyvalues7_15retrainingMA.png",p)
 
-data = load("movie_audio_classification/data/shapleyvalues17_15retraining.jld2")
-p = shapvalues17_plot(data, L"Modality classification$$","png")
-display(p)
-save("movie_audio_classification/plots/new/shapleyvalues17_15retrainingMA.png",p)
+# data7 = load("movie_audio_classification/data/shapleyvalues7_15retraining_changeshuffling.jld2")
+# p = shapvalues_plot(data7, L"Modality classification$$","png")
+# display(p)
+# save("movie_audio_classification/plots/new/shapleyvalues7_15retrainingMA.png",p)
+
+# data = load("movie_audio_classification/data/shapleyvalues17_15retraining.jld2")
+# p = shapvalues17_plot(data, L"Modality classification$$","png")
+# display(p)
+# save("movie_audio_classification/plots/new/shapleyvalues17_15retrainingMA.png",p)
+maxs = zeros(5)
+mins = zeros(5)
+mean_val = zeros(5*7)
+k =1
+for i in 1:5
+    data = JLD2.load("movie_audio_classification/data/shapleyvalues10random_15retraining_$(i).jld2")
+   # p = shapvaluesrandom_plot(data, L"Content classification$$","png")
+   data= data["d"]
+    for key in  ["r1", "r2" , "r3", "r4", "r5", "r6", "r7"]
+        global k
+        mean_val[k] = mean(data[key])
+        k +=1
+    end
+end
 
