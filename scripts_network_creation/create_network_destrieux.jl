b142f79d47f8c53d1940b069ea93cba43cf4aee5
aurorarossi <aurora98rossi@gmail.com>
2024-09-16 16:04:08 +0200

    Add destrieux

---
+++ scripts_network_creation/create_network_destrieux.jl
@@ -0,0 +1,28 @@
+using NPZ, Statistics, DelimitedFiles
+
+names = readdlm("files/to_process/to_process_schema4.txt")
+for l in 1:length(names)
+    println(names[l])
+    name = names[l]
+    _,_,c = split(name,"/")
+    timeseries = npzread("timeseries/schema-run4-100/$(c)_timeseries_destrieux.npy")
+
+    T = 20
+    num_regions = 75
+    global k=1
+    window = collect(1:15:(size(timeseries,2)-20))
+    network = zeros(num_regions,num_regions,length(window))
+   
+
+    for t in window
+        global k
+        for i in 1:num_regions
+            for j in 1:num_regions
+                network[i,j,k]=cor(timeseries[i,t:t+T], timeseries[j,t:t+T])
+            end
+        end
+        k+=1
+    end
+
+    npzwrite("data/desikan/$(c)_network_destrieux.npy", network)
+end
