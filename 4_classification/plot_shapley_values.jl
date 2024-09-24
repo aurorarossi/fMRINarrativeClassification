3e6fbec9b0a0dc0e8dc4fe67ce313e67f1c3ff97
Aurora Rossi <aurora98rossi@gmail.com>
2024-09-24 15:47:14 +0200

    New images

--- 4_classification/plot_shapley_values.jl
+++ 4_classification/plot_shapley_values.jl
@@ -1,13 +1,29 @@
 using JLD2
 
 include("../plots/plot.jl")
-include("../t-test/t-test_17.jl")
-include("../t-test/t-test.jl")
 
 data = load("4_classification/data/shapleyvalues7_15retraining.jld2")
-p = shapvalues_plot(data, L"Combined Modality and Content classiﬁcation$$","png")
-save("4_classification/plots/new/shapleyvalues7_15retraining4C.png",p)
+p = shapvalues_plot(data, L"Combined Modality and Content classification$$","png")
+save("4_classification/plots/shapleyvalues7_15retraining4C.png",p)
 
 data17 = load("4_classification/data/shapleyvalues17_15retraining.jld2")
-p17 = shapvalues17_plot(data17, L"Combined Modality and Content classiﬁcation$$","png")
-save("4_classification/plots/new/shapleyvalues17_15retraining4C.png",p17)
+p17 = shapvalues17_plot(data17, L"Combined Modality and Content classification$$","png")
+save("4_classification/plots/shapleyvalues17_15retraining4C.png",p17)
+
+data100_5 = load("4_classification/data/shapleyvalues100_5retraining.jld2")
+data100_10 = load("4_classification/data/shapleyvalues100_10retraining.jld2")
+d5 = data100_5["d"]
+d10= data100_10["d"]
+
+data100 = Dict(key => vcat(d5[key],d10[key]) for key in keys(d5))
+
+p100 = shapvalues100_plot(data100,L"Combined Modality and Content classification$$","png")
+save("4_classification/plots/shapleyvalues100_15retraining4C.png",p100)
+
+datadesikan = load("4_classification/data/shapleyvalues70_desikan_15retraining.jld2")
+pdesikan = shapvaluesdesikan_plot(datadesikan, L"Combined Modality and Content classification$$","png")
+save("4_classification/plots/shapleyvalues70_desikan_15retraining4C.png",pdesikan)
+
+datadestrieux = load("4_classification/data/shapleyvalues75_destrieux_15retraining.jld2")
+pdestrieux = shapvaluesdestrieux_plot(datadestrieux, L"Combined Modality and Content classification$$","png")
+save("4_classification/plots/shapleyvalues75_destrieux_15retraining4C.png",pdestrieux)
--- airport_restaurant_classification/plot_shapley_values.jl
+++ airport_restaurant_classification/plot_shapley_values.jl
@@ -1,17 +1,28 @@
 using JLD2
 include("../plots/plot.jl")
-include("../t-test/t-test_17.jl")
-include("../t-test/t-test.jl")
 
-data7 = load("airport_restaurant_classification/data/shapleyvalues7_15retraining.jld2")
-p = shapvalues_plot(data, L"Content classification$$","png")
-save("airport_restaurant_classification/plots/new/shapleyvalues7_15retrainingAR.png",p)
+data = load("airport_restaurant_classification/data/shapleyvalues7_15retraining.jld2")
+p = shapvalues_plot(data, L"Content classification$$", "png")
+save("airport_restaurant_classification/plots/shapleyvalues7_15retrainingAR.png", p)
 
 data17 = load("airport_restaurant_classification/data/shapleyvalues17_15retraining.jld2")
-p17 = shapvalues17_plot(data17, L"Content classification$$","png")
-save("airport_restaurant_classification/plots/new/shapleyvalues17_15retraining.png",p17)
+p17 = shapvalues17_plot(data17, L"Content classification$$", "png")
+save("airport_restaurant_classification/plots/shapleyvalues17_15retrainingAR.png", p17)
 
-data17 = load("airport_restaurant_classification/data/shapleyvalues17_15retraining_500sample.jld2")
-p17 = shapvalues17_plot(data17, L"Content classification 500 samples$$","png")
-save("airport_restaurant_classification/plots/new/shapleyvalues17_15retrainingAR_500sample.png",p17)
+data100_5 = load("airport_restaurant_classification/data/shapleyvalues100_5retraining.jld2")
+data100_10 = load("airport_restaurant_classification/data/shapleyvalues100_10retraining.jld2")
+d5 = data100_5["d"]
+d10 = data100_10["d"]
 
+data100 = Dict(key => vcat(d5[key], d10[key]) for key in keys(d5))
+
+p100 = shapvalues100_plot(data100, L"Content classification$$", "png")
+save("airport_restaurant_classification/plots/shapleyvalues100_15retrainingAR.png", p100)
+
+datadesikan = load("airport_restaurant_classification/data/shapleyvalues70_desikan_15retraining.jld2")
+pdesikan = shapvaluesdesikan_plot(datadesikan, L"Content classification$$", "png")
+save("airport_restaurant_classification/plots/shapleyvalues70_desikan_15retrainingAR.png", pdesikan)
+
+datadestrieux = load("airport_restaurant_classification/data/shapleyvalues75_destrieux_15retraining.jld2")
+pdestrieux = shapvaluesdestrieux_plot(datadestrieux, L"Content classification$$", "png")
+save("airport_restaurant_classification/plots/shapleyvalues75_destrieux_15retrainingAR.png", pdestrieux)
